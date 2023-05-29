3e56279001f78d2c3f78f16a44b94e8a59586c6b
K <kihyuk.sung@outlook.kr>
2023-05-29 13:36:48 +0900

    jamo plugin 추가
    - jamo yamlRestTest 추가

--- src/main/java/org/example/es/plugins/ESPlugins.java
+++ src/main/java/org/example/es/plugins/ESPlugins.java
@@ -5,14 +5,20 @@ import org.elasticsearch.indices.analysis.AnalysisModule;
 import org.elasticsearch.plugins.AnalysisPlugin;
 import org.elasticsearch.plugins.Plugin;
 import org.example.es.plugins.chosung.ChosungTokenFilterFactory;
+import org.example.es.plugins.jamo.JamoTokenFilterFactory;
 
-import java.util.Collections;
+import java.util.HashMap;
 import java.util.Map;
 
 public class ESPlugins extends Plugin implements AnalysisPlugin {
 
     @Override
     public Map<String, AnalysisModule.AnalysisProvider<TokenFilterFactory>> getTokenFilters() {
-        return Collections.singletonMap("chosung", ChosungTokenFilterFactory::new);
+        Map<String, AnalysisModule.AnalysisProvider<TokenFilterFactory>> extra = new HashMap<>();
+
+        extra.put("chosung", ChosungTokenFilterFactory::new);
+        extra.put("jamo", JamoTokenFilterFactory::new);
+
+        return extra;
     }
 }
