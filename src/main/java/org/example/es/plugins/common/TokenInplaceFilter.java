afadde677d44e405d9a294ce6f751c3ac506b859
K <kihyuk.sung@outlook.kr>
2023-05-29 12:57:29 +0900

    TokenFilter 구현체의 incrementToken final로 수정
    - 해당 메서드는 final이어야 함

--- src/main/java/org/example/es/plugins/common/TokenInplaceFilter.java
+++ src/main/java/org/example/es/plugins/common/TokenInplaceFilter.java
@@ -17,7 +17,7 @@ public class TokenInplaceFilter extends TokenFilter {
     }
 
     @Override
-    public boolean incrementToken() throws IOException {
+    public final boolean incrementToken() throws IOException {
         if (!input.incrementToken()) {
             return false;
         }
