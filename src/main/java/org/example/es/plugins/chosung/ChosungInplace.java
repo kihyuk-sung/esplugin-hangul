a55217a4a3df733bb33a00d899342a520e7965ad
K <kihyuk.sung@outlook.kr>
2023-05-29 14:10:12 +0900

    ChosungInplace 내에서 KoreanUtils 사용하도록 수정
    - JamoConvert 등에서도 공통으로 사용하므로, KoreanUtils를 사용

--- src/main/java/org/example/es/plugins/chosung/ChosungInplace.java
+++ src/main/java/org/example/es/plugins/chosung/ChosungInplace.java
@@ -1,31 +1,14 @@
 package org.example.es.plugins.chosung;
 
+import org.example.es.plugins.common.KoreanUtils;
 import org.example.es.plugins.common.TokenInplace;
 
 public class ChosungInplace implements TokenInplace {
 
-    private static final char HANGUL_SYLLABLES_START = '가';
-    private static final int INITIAL_CONSONANT_BASE = 28 * 21;
-    private static final char HANGUL_SYLLABLES_END = '힣';
-    private static final char[] initialConsonants = {
-            'ㄱ', 'ㄲ', 'ㄴ', 'ㄷ', 'ㄸ', 'ㄹ', 'ㅁ', 'ㅂ', 'ㅃ', 'ㅅ', 'ㅆ', 'ㅇ', 'ㅈ', 'ㅉ', 'ㅊ', 'ㅋ', 'ㅌ', 'ㅍ', 'ㅎ'
-    };
-
     @Override
     public char inplace(char ch) {
-        if (HANGUL_SYLLABLES_START <= ch && ch <= HANGUL_SYLLABLES_END) {
-            return inplaceHangulToChosung(ch);
-        }
-
-        return ch;
-    }
-
-    private char inplaceHangulToChosung(final char ch) {
-        int charIndex = ch - HANGUL_SYLLABLES_START;
-        int initialConsonantIndex = charIndex / INITIAL_CONSONANT_BASE;
-
-        if (initialConsonantIndex < initialConsonants.length) {
-            return initialConsonants[initialConsonantIndex];
+        if (KoreanUtils.isHangulSyllables(ch)) {
+            return KoreanUtils.getHangulInitialConsonant(ch);
         }
 
         return ch;
