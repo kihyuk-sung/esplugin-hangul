1c7a422e23b79c34fdee47d63990f88c8da6d674
K <kihyuk.sung@outlook.kr>
2023-05-29 12:57:28 +0900

    TokenConvertFilter
    - lucene의 TokenFilter를 상속받음
    - TokenFilter의 동작은 termAttr를 변경하는 형태로 동작한다.
    - 이때 토큰 내용을 완전히 변경하는 경우 (ex. 가나다 -> ㄱㅏㄴㅏㄷㅏ)
    - 또는 글자를 inplace 변경하는 경우가 있다. (ex. abc -> ABC)
    - 첫번째 경우는 깔끔하게 setEmpty 후 append를 사용한다.
    - 두번째 경우는 CharTermAttribute를 직접 조작하는 것도 하나의 방법이다.
    - 첫번째 경우를 위한 구현체이다.

---
+++ src/main/java/org/example/es/plugins/common/TokenConvert.java
@@ -0,0 +1,5 @@
+package org.example.es.plugins.common;
+
+public interface TokenConvert {
+    String convert(CharSequence token);
+}
---
+++ src/main/java/org/example/es/plugins/common/TokenConvertFilter.java
@@ -0,0 +1,29 @@
+package org.example.es.plugins.common;
+
+import org.apache.lucene.analysis.TokenFilter;
+import org.apache.lucene.analysis.TokenStream;
+import org.apache.lucene.analysis.tokenattributes.CharTermAttribute;
+
+import java.io.IOException;
+
+public class TokenConvertFilter extends TokenFilter {
+
+    private final TokenConvert tokenConvert;
+    private final CharTermAttribute termAttr = addAttribute(CharTermAttribute.class);
+
+    public TokenConvertFilter(TokenStream input, TokenConvert tokenConvert) {
+        super(input);
+        this.tokenConvert = tokenConvert;
+    }
+
+    @Override
+    public boolean incrementToken() throws IOException {
+        if (!input.incrementToken()) {
+            return false;
+        }
+
+        termAttr.setEmpty();
+        termAttr.append(tokenConvert.convert(termAttr));
+        return true;
+    }
+}
