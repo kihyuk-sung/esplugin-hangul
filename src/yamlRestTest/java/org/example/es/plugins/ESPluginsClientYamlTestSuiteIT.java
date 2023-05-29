c6e43d10a387224f441ad28b4e646688c938d0b0
K <kihyuk.sung@outlook.kr>
2023-05-29 12:57:29 +0900

    yamlRestTest 적용
    - yamlRestTest를 이용하여 es plugin 등록 및 동작 테스트 가능
    - yamlRestTestRuntimeOnly로 로깅 관련 의존성 추가해야 동작함

---
+++ src/yamlRestTest/java/org/example/es/plugins/ESPluginsClientYamlTestSuiteIT.java
@@ -0,0 +1,17 @@
+package org.example.es.plugins;
+
+import com.carrotsearch.randomizedtesting.annotations.Name;
+import com.carrotsearch.randomizedtesting.annotations.ParametersFactory;
+import org.elasticsearch.test.rest.yaml.ClientYamlTestCandidate;
+import org.elasticsearch.test.rest.yaml.ESClientYamlSuiteTestCase;
+
+public class ESPluginsClientYamlTestSuiteIT extends ESClientYamlSuiteTestCase {
+    public ESPluginsClientYamlTestSuiteIT(@Name("yaml") ClientYamlTestCandidate testCandidate) {
+        super(testCandidate);
+    }
+
+    @ParametersFactory
+    public static Iterable<Object[]> parameters() throws Exception {
+        return ESClientYamlSuiteTestCase.createParameters();
+    }
+}
