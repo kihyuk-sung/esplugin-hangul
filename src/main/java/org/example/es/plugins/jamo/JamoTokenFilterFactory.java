f2f1ac56d27c41a557c58cb2504b27056f624656
K <kihyuk.sung@outlook.kr>
2023-05-29 12:57:29 +0900

    JamoTokenFilterFactory 추가

---
+++ src/main/java/org/example/es/plugins/jamo/JamoTokenFilterFactory.java
@@ -0,0 +1,21 @@
+package org.example.es.plugins.jamo;
+
+import org.apache.lucene.analysis.TokenStream;
+import org.elasticsearch.common.settings.Settings;
+import org.elasticsearch.env.Environment;
+import org.elasticsearch.index.IndexSettings;
+import org.elasticsearch.index.analysis.AbstractTokenFilterFactory;
+import org.example.es.plugins.common.TokenConvertFilter;
+
+public class JamoTokenFilterFactory extends AbstractTokenFilterFactory {
+
+    private static final JamoConvert convert = new JamoConvert();
+    public JamoTokenFilterFactory(IndexSettings indexSettings, Environment env, String name, Settings settings) {
+        super(indexSettings, name, settings);
+    }
+
+    @Override
+    public TokenStream create(TokenStream tokenStream) {
+        return new TokenConvertFilter(tokenStream, convert);
+    }
+}
